3e114a8d7f6de566e5bf45b8d02cfc370993abf4
RCowles <contact@ryanscowles.com>
2014-12-03 23:28:47 -0800

    Let's move the_content for the front-page into welcome.php... I'm a few beers in, so might revert this

--- welcome.php
+++ welcome.php
@@ -11,8 +11,17 @@
 		<?php endif; // End header image check. ?>
 		*/ ?>
 		<div class="welcome-message">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php the_content(); ?>
+
+			<?php endwhile; // end of the loop. ?>
+
+			<?php /*
 			<h1>Hello. I'm Ryan. I build websites with WordPress.</h1>
 			<p><a href="#" class="button large-btn">Get in touch</a></p>
+			*/ ?>
 		</div><!-- .welcome-message !-->
 
 	</section>
