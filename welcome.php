d1a0ec0b6b2f7590df04b556005e2189b3114524
RCowles <contact@ryanscowles.com>
2014-12-04 16:58:36 -0800

    Remove old static text from welcome.php

--- welcome.php
+++ welcome.php
@@ -18,10 +18,6 @@
 
 			<?php endwhile; // end of the loop. ?>
 
-			<?php /*
-			<h1>Hello. I'm Ryan. I build websites with WordPress.</h1>
-			<p><a href="#" class="button large-btn">Get in touch</a></p>
-			*/ ?>
 		</div><!-- .welcome-message !-->
 
 	</section>
