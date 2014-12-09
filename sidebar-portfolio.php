c5ba53580eb87c89e8769b8889a28a8ce6b76712
RCowles <contact@ryanscowles.com>
2014-12-09 00:06:01 -0800

    Make items in cat list and tag list different colors

--- sidebar-portfolio.php
+++ sidebar-portfolio.php
@@ -10,17 +10,19 @@
 <div id="secondary" class="widget-area portfolio-sidebar" role="complementary">
 
 	<div class="entry-meta">
+		<div class="widget">
+			<h3><?php _e( 'Type:', 'looseleaf' ); ?></h3>
+			<?php
+				echo get_the_term_list( $post->ID, 'jetpack-portfolio-type', '<ul class="portfolio-type-links"><li>', '</li><li>', '</li></ul>' );
+			?>
+		</div><!-- .widget !-->
 
-		<h3><?php _e( 'Type:', 'looseleaf' ); ?></h3>
-		<?php
-			echo get_the_term_list( $post->ID, 'jetpack-portfolio-type', '<ul class="portfolio-type-links"><li>', '</li><li>', '</li></ul>' );
-		?>
-
-		<h3><?php _e( 'Tags:', 'looseleaf' ); ?></h3>
-		<?php
-			echo get_the_term_list( $post->ID, 'jetpack-portfolio-tag', '<ul class="tags-links"><li>', '</li><li>', '</li></ul>' );
-		?>
-
+		<div class="widget">
+			<h3><?php _e( 'Tags:', 'looseleaf' ); ?></h3>
+			<?php
+				echo get_the_term_list( $post->ID, 'jetpack-portfolio-tag', '<ul class="tags-links"><li>', '</li><li>', '</li></ul>' );
+			?>
+		</div><!-- .widget !-->
 	</div><!-- .entry-meta !-->
 
 	<?php looseleaf_post_nav(); ?>
