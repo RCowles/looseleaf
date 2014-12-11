46e56fcd0c3a930965d1b8dad51801e4010357e3
RCowles <contact@ryanscowles.com>
2014-12-10 23:40:17 -0800

    Add some basic breakpoints to main content area

--- sidebar-portfolio.php
+++ sidebar-portfolio.php
@@ -10,17 +10,17 @@
 <div id="secondary" class="widget-area portfolio-sidebar" role="complementary">
 
 	<div class="entry-meta">
-		<div class="widget">
+		<div class="widget cat-links">
 			<h3><?php _e( 'Type:', 'looseleaf' ); ?></h3>
 			<?php
-				echo get_the_term_list( $post->ID, 'jetpack-portfolio-type', '<ul class="portfolio-type-links"><li>', '</li><li>', '</li></ul>' );
+				echo get_the_term_list( $post->ID, 'jetpack-portfolio-type', '<span class="dashicons dashicons-category"></span>', '<span class="dashicons dashicons-category"></span>', ' ' );
 			?>
 		</div><!-- .widget !-->
 
-		<div class="widget">
+		<div class="widget tags-links">
 			<h3><?php _e( 'Tags:', 'looseleaf' ); ?></h3>
 			<?php
-				echo get_the_term_list( $post->ID, 'jetpack-portfolio-tag', '<ul class="tags-links"><li>', '</li><li>', '</li></ul>' );
+				echo get_the_term_list( $post->ID, 'jetpack-portfolio-tag', '<span class="dashicons dashicons-tag"></span>', '<span class="dashicons dashicons-tag"></span>', ' ' );
 			?>
 		</div><!-- .widget !-->
 	</div><!-- .entry-meta !-->
