45eea57571587942102e5557c083a4b9fb662f85
RCowles <contact@ryanscowles.com>
2015-02-14 17:50:06 -0800

    Create Jetpack Testimonial archive page template

--- content-testimonial.php
+++ content-testimonial.php
@@ -7,6 +7,9 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( '<h3 class="entry-title">', '</h3>' ); ?>
+	</header><!-- .entry-header -->
 
 	<div class="entry-content">
 		<?php the_content(); ?>
